995d045f2e9044751e6c8e9ae569b27b05ec1471
Ashraf Ali.Shaik <ashrafalis711@gmail.com>
2024-08-16 22:52:41 +0530

    Initial commit from local

---
+++ src/main/java/com/analysis/knowledge/RestMongoDB/RestMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.analysis.knowledge.RestMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analysis/knowledge/RestMongoDB/RestMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analysis.knowledge.RestMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
